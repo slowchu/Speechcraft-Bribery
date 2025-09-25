0e8a1cf4cb3b1e02ea3fa101e1464bac21bad809
slowchu <134579951+slowchu@users.noreply.github.com>
2025-09-25 15:32:55 -0700

    Delete Speechcraft Bribery directory

--- Speechcraft Bribery/scripts/speechcraft_bribe/ui_close_on_global.lua
+++
@@ -1,8 +0,0 @@
--- scripts/speechcraft_bribe/ui_close_on_global.lua
--- Optional tiny helper: ensure any dangling UI gets closed if dialogue ends due to global events.
-local uiGlue = require("scripts/speechcraft_bribe.ui")
-return {
-  eventHandlers = {
-    ForceCloseBribeUi = function() uiGlue.close() end,
-  }
-}
